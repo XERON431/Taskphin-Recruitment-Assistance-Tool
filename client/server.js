2c6913d21783881d3db8eddc2f091d0bfcf89778
Siddhant Goyal <103522839+XERON431@users.noreply.github.com>
2024-02-19 14:18:19 +0530

    Update server.js

--- client/server.js
+++ client/server.js
@@ -15,7 +15,7 @@ app.prepare()
       server.use(
         '/api',
         createProxyMiddleware({
-          target: 'http://localhost:8001', // Your server address
+          target: 'https://recruitment-assistance-webapp.onrender.com', // Your server address
           changeOrigin: true,
         })
       );
